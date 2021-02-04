2b5fb2a097e4483a9055be939e14efab405b09e6
abarnatan <ale.barn19@gmail.com>
2021-02-03 17:25:51 -0800

    adding files, put in apis and querying url

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,9 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+    // The data.features object is in the GeoJSON standard
+    console.log(data.features);
+
+});
