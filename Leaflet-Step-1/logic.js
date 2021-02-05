1ae2968a23c35b0b3349ca26f312ab7cab57536e
abarnatan <ale.barn19@gmail.com>
2021-02-04 16:24:26 -0800

    markers are working with popups as well!

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
@@ -10,6 +10,11 @@ d3.json(queryUrl).then(function(data) {
   // each type of feature (held in the `geometry` key) and draws the correct markers.
   var earthquakes = L.geoJSON(data.features);
 
+      var earthquakes = L.geoJSON(data.features, {
+        onEachFeature: onEachFeatureFunc,
+        pointToLayer: pointToLayerFunc
+      });
+
   // The rest of this is all the same
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -18,14 +23,14 @@ d3.json(queryUrl).then(function(data) {
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  });
+});
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
     accessToken: API_KEY
-  });
+});
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -60,23 +65,23 @@ d3.json(queryUrl).then(function(data) {
 // // L.geoJSON() also gives us handy options, almost like a built in `.forEach()`
 // // Define a function we want to run once for each feature in the features array
 // // Give each feature a popup describing the place and time of the earthquake
-// function onEachFeatureFunc(feature, layer) {
-//   layer.bindPopup("<h3>" + feature.properties.place +
-//     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-// }
+function onEachFeatureFunc(feature, layer) {
+  layer.bindPopup("<h3>" + feature.properties.place +
+    "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+}
 
-// var geojsonMarkerOptions = {
-//   radius: 8,
-//   fillColor: "#ff7800",
-//   color: "#000",
-//   weight: 1,
-//   opacity: 1,
-//   fillOpacity: 0.8
-// };
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
 
-// function pointToLayerFunc(feature, latlng) {
-//   return L.circleMarker(latlng, geojsonMarkerOptions);
-// }
+function pointToLayerFunc(feature, latlng) {
+  return L.circleMarker(latlng, geojsonMarkerOptions);
+}
 
 
 // // Create a GeoJSON layer containing the features array on the earthquakeData object
